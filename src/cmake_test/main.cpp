4b543471baa88f8c38175fdf4f7f40d58034063e
AlexandruDaniel <barbualex.daniel2004@gmail.com>
2025-10-28 17:32:57 +0100

    Added glm to my project and a small cmake test folder.
    Signed-off-by: AlexandruDaniel <barbualex.daniel2004@gmail.com>

---
+++ src/cmake_test/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <glm/glm.hpp>
+
+int main() {
+    glm::vec3 v(1.0f, 1.0f, 1.0f);
+    std::cout << "Hello, CMake!" << std::endl;
+    std::cout << v.x;
+    return 0;
+}
