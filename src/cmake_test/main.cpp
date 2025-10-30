49dbfbcc9e57da6698897b5a406e0b608d2cd90e
AlexandruDaniel <barbualex.daniel2004@gmail.com>
2025-10-30 22:59:41 +0100

    Testing GLFW
    Had problems with CMake but i am making progress.
    
    Signed-off-by: AlexandruDaniel <barbualex.daniel2004@gmail.com>

--- src/cmake_test/main.cpp
+++ src/cmake_test/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
 
 int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+    
     glm::vec3 v(1.0f, 1.0f, 1.0f);
     std::cout << "Hello, CMake!" << std::endl;
     std::cout << v.x;
