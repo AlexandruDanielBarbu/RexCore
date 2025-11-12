dc5ecb8f4a4750b99680e12b7d1d42d763037784
AlexandruDaniel <barbualex.daniel2004@gmail.com>
2025-11-13 00:11:50 +0100

    Added some coding style to my code via .clang-format file
    Signed-off-by: AlexandruDaniel <barbualex.daniel2004@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
 #include "Log.hpp"
+#include <iostream>
 
 int main() {
-    REX_PRINT("bunaaaaa");
-    REX_LOG("Buna! Eu sunt RexCore");
-    REX_ERROR("Iar asta este o eroare");
-    return 0;
+        REX_PRINT("bunaaaaa");
+        REX_LOG("Buna! Eu sunt RexCore");
+        REX_ERROR("Iar asta este o eroare");
+        return 0;
 }
