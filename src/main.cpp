1751364ac8b900735d9af0aa876248ffbbc4b053
AlexandruDaniel <barbualex.daniel2004@gmail.com>
2025-11-14 19:12:14 +0100

    Added first bit of code logic: creating a window using glfw.
    Signed-off-by: AlexandruDaniel <barbualex.daniel2004@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,58 @@
-#include "Log.hpp"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
+
+#include "Log.hpp"
+
+class HelloTriangle {
+      public:
+        void run() {
+                initVulkan();
+                mainLoop();
+                cleanup();
+        }
+
+      private:
+        const uint32_t WIDTH     = 800;
+        const uint32_t HEIGHT    = 800;
+        const char*    APP_TITLE = "RexCore_triangle";
+
+        GLFWwindow* window = nullptr;
+
+        void initVulkan() {
+                glfwInit();
+
+                // cut opengl out from now on
+                glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+                // forget about resizing for now
+                glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+                window = glfwCreateWindow(WIDTH, HEIGHT, APP_TITLE, nullptr,
+                                          nullptr);
+        }
+
+        void mainLoop() {
+                while (!glfwWindowShouldClose(window)) {
+                        glfwPollEvents();
+                }
+        }
+
+        void cleanup() {
+                glfwDestroyWindow(window);
+                glfwTerminate();
+        }
+};
 
 int main() {
-        REX_PRINT("bunaaaaa");
-        REX_LOG("Buna! Eu sunt RexCore");
-        REX_ERROR("Iar asta este o eroare");
-        return 0;
+        HelloTriangle mainApp;
+        try {
+                mainApp.run();
+        } catch (const std::exception& e) {
+                REX_ERROR(e.what());
+                return EXIT_FAILURE;
+        }
+        return EXIT_SUCCESS;
 }
