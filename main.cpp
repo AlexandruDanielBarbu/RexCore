96630279d4586184730fa5b697524b024f6410e9
AlexandruDaniel <186506659+AlexandruDanielBarbu@users.noreply.github.com>
2026-03-13 14:21:45 +0200

    Created the vulkan Instance. Moving on to the validation layers (again).
    Signed-off-by: AlexandruDaniel <186506659+AlexandruDanielBarbu@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>  // report and propagate erros
 #include <stdexcept> // report and propagate erros
 #include <cstdlib>   // EXIT_FAILURE; EXIT_SUCCESS
+#include <algorithm> // ranges
 
 constexpr uint32_t WIDTH          = 800;
 constexpr uint32_t HEIGHT         = 800;
@@ -36,6 +37,41 @@ class Engine
 		window = glfwCreateWindow(WIDTH, HEIGHT, WINDOW_TITLE, nullptr, nullptr);
 	}
 	void initVulkan() {
+		createInstance();
+	}
+	vk::raii::Context context;
+	vk::raii::Instance instance = nullptr;
+	void createInstance() {
+		// Engine specific cnfiguration
+		constexpr vk::ApplicationInfo appInfo {
+			.pApplicationName = "Hello Trig",
+			.applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+		        .pEngineName        = "Rex Core Engine",
+			.engineVersion      = VK_MAKE_VERSION(1, 0, 0),
+			.apiVersion         = vk::ApiVersion14
+		};
+
+		// Get and check GLFW extensions
+	        uint32_t glfwExtensionCount = 0;
+		const char **glfwExtensions  = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		auto extensionPrperties = context.enumerateInstanceExtensionProperties();
+		for (uint32_t i = 0; i < glfwExtensionCount; i++)
+		{
+			if (std::ranges::none_of(
+				extensionPrperties,
+				[glfwExtension = glfwExtensions[i]](const auto& extensionProperty) {
+					return strcmp(glfwExtension, extensionProperty.extensionName) == 0; })) {
+		                throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+			}
+		}
+		vk::InstanceCreateInfo createInfo {
+			.pApplicationInfo = &appInfo,
+			.enabledExtensionCount = glfwExtensionCount,
+			.ppEnabledExtensionNames = glfwExtensions
+		};
+
+		instance = vk::raii::Instance(context, createInfo);
 	}
 	void mainLoop() {
 		while (!glfwWindowShouldClose(window)) {
