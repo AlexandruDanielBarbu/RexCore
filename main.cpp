dcf139148359c6c84d5264e14059df0495a53ad7
AlexandruDaniel <186506659+AlexandruDanielBarbu@users.noreply.github.com>
2026-03-13 13:44:48 +0200

    Starting fresh with vulkan.hpp
    Signed-off-by: AlexandruDaniel <186506659+AlexandruDanielBarbu@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,61 +1,49 @@
-#include <memory>
-#if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
-#	include <vulkan/vulkan_raii.hpp>
-#else
-import vulkan_hpp;
-#endif
+#define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
+#include <vulkan/vulkan_raii.hpp>
+
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include <cstdlib>
-#include <iostream>
-#include <stdexcept>
+#include <iostream>  // report and propagate erros
+#include <stdexcept> // report and propagate erros
+#include <cstdlib>   // EXIT_FAILURE; EXIT_SUCCESS
 
-const uint32_t WIDTH  = 800;
-const uint32_t HEIGHT = 600;
+constexpr uint32_t WIDTH          = 800;
+constexpr uint32_t HEIGHT         = 800;
+constexpr char     WINDOW_TITLE[] = "Rex Core";
 
-class HelloTriangleApplication
+class Engine
 {
   public:
-	void run()
-	{
-		std::cout << "Hello!!!!" << std::endl;
+	void run() {
 		initWindow();
 		initVulkan();
 		mainLoop();
 		cleanup();
 	}
-
   private:
-	GLFWwindow *window = nullptr;
-
-	void initWindow()
-	{
+	GLFWwindow *window;
+	void initWindow() {
 		glfwInit();
 
+		// No OpenGL
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		
+		// Window resize turned off
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+		// Creating the Window
+		window = glfwCreateWindow(WIDTH, HEIGHT, WINDOW_TITLE, nullptr, nullptr);
 	}
-
-	void initVulkan()
-	{
+	void initVulkan() {
 	}
-
-	void mainLoop()
-	{
-		while (!glfwWindowShouldClose(window))
-		{
+	void mainLoop() {
+		while (!glfwWindowShouldClose(window)) {
 			glfwPollEvents();
-			std::cout << "This is a test!!!" << std::endl;
-
 		}
 	}
-
-	void cleanup()
-	{
+	void cleanup() {
 		glfwDestroyWindow(window);
-
 		glfwTerminate();
 	}
 };
@@ -64,7 +52,7 @@ int main()
 {
 	try
 	{
-		HelloTriangleApplication app;
+		Engine app;
 		app.run();
 	}
 	catch (const std::exception &e)
